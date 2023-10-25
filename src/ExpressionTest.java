7ea5e8a505626c8c388c97b28b6e5979e227c203
Rick M <2545827+rick-paaz@users.noreply.github.com>
2023-10-24 23:33:05 -0700

    Update ExpressionTest.java

--- src/ExpressionTest.java
+++ src/ExpressionTest.java
@@ -9,9 +9,7 @@ class ExpressionTest {
   Expressions e = new Expressions();
 
   @Test
-  void testIn2PostFromSlides() {
-    assertEquals("2 3 4 + 2 6 - / *", e.inToPost("2 * ( ( 3 + 4 ) / 2 - 6 )"));
-    // 7 / -4 == -1
-    assertEquals(-2, e.valueOf("2 3 4 + 2 6 - / *"));
+  void testValueOf() {
+    assertEquals(5, e.valueOf("2 3 +"));
   }
 }
